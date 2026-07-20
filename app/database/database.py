295abe33c2e7a19fc0d5f7b029dbf4be39f3ba1a
unknown <matin.mtn.mh@gmail.com>
2026-07-20 17:20:39 +0330

    create database setup

--- app/database/database.py
+++ app/database/database.py
@@ -6,7 +6,7 @@ from app.core.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(DATABASE_URL, check_same_thread=False)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 sessionlocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
