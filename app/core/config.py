d6c13644710e2adbff0eb4ff240e987d9d7f68e0
unknown <matin.mtn.mh@gmail.com>
2026-07-20 16:58:02 +0330

    update project config settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
 
     SECRET_KEY: str
     DATABASE_URL: str
     JWT_ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ADMIN_USERNAME: str
+    ADMIN_EMAIL: str
+    ADMIN_PASSWORD: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
